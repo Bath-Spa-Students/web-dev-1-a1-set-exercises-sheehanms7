26998818d058ea85183d3fdea1e4a68de705b93d
sheehanms7 <sheehanms77@gmail.com>
2024-04-03 23:01:02 +0400

    chapter 5

---
+++ Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/index.js
@@ -0,0 +1,40 @@
+//AHAA
+function aha(){
+    var audio=document.getElementById("sound1")
+    sound1.play();
+}
+//Dan
+function dan(){
+    var audio=document.getElementById("sound2")
+    sound2.play();
+}
+//BACK
+function Back(){
+    var audio=document.getElementById("sound3")
+    sound3.play();
+}
+//BANG
+function Bang(){
+    var audio=document.getElementById("sound4")
+    sound4.play();
+}
+//IM
+function Im(){
+    var audio=document.getElementById("sound5")
+    sound5.play();
+}
+//SCOTCH EGG
+function Scotch(){
+    var audio=document.getElementById("sound6")
+    sound6.play();
+}
+//EMAIL
+function EMAIL(){
+    var audio=document.getElementById("sound7")
+    sound7.play();
+}
+//HELLO
+function HELLO(){
+    var audio=document.getElementById("sound8")
+    sound8.play();
+}
