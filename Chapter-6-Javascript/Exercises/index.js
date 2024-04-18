c23b473eb42bfe955754a380947bcb1e85a178e1
sheehanms7 <sheehanms77@gmail.com>
2024-04-18 12:27:53 +0400

    Chapter 6

--- Chapter-6-Javascript/Exercises/index.js
+++ Chapter-6-Javascript/Exercises/index.js
@@ -1,1 +1,8 @@
+document.getElementById("calculateButton").addEventListener("click", function() {
+    var costPerLiter = parseFloat(document.getElementById("cost").value);
+    var liters = parseFloat(document.getElementById("liters").value);
+    
     var totalCostAed = costPerLiter * liters;
+    
+    document.getElementById("totalCost").innerText = "Total Cost: AED " + totalCostAed.toFixed(2);
+  });
